abca4d963b45a2e273e2fd3de53d1e08a828c89e
mathi <mathi@130.66.206.254>
2024-10-07 15:19:32 +0200

    test commit

---
+++ TP-poo/src/main/java/md/tp/poo/TPPoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package md.tp.poo;
+
+/**
+ *
+ * @author mathi
+ */
+public class TPPoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
