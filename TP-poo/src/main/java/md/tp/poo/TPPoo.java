1adcd83e9f18b1063e410edfdba03b07dd886391
Tatsuya0924 <wootatsuya0924@gmail.com>
2024-10-07 15:23:08 +0200

    finished

--- TP-poo/src/main/java/md/tp/poo/TPPoo.java
+++ TP-poo/src/main/java/md/tp/poo/TPPoo.java
@@ -11,6 +11,7 @@ package md.tp.poo;
 public class TPPoo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello!");
+        
     }
 }
